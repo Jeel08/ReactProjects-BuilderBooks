15ce50156720ccd8ff25c44103c1c73b82fd01cb
Jeel08 <panchaljeel786@gmail.com>
2024-10-18 19:04:22 +0530

    New Array Updated

---
+++ src/component/author.jsx
@@ -0,0 +1,10 @@
+import auth1 from '../images/auther-1.png';
+import auth2 from '../images/auther-2.png';
+const Author = () => {
+
+    const auth = [
+        {img:auth1,name:"Kelly"},
+        {img:auth2,name:"Tim"}
+    ]
+}
+export default Author;
--- src/component/singup.jsx
+++ src/component/singup.jsx
@@ -1,3 +1,4 @@
 const Singup = ()=> {
-    
+
 }
+export default Singup;
