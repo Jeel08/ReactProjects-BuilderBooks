6713acf69c98bd98187e5f9ca49336526115d598
Jeel08 <panchaljeel786@gmail.com>
2024-10-18 18:46:24 +0530

    Builder Design

---
+++ src/component/imgCard.jsx
@@ -0,0 +1,14 @@
+import img1 from '../images/image-1.jpeg';
+import img2 from '../images/image-2.jpg';
+import img3 from '../images/image-3.jpg';
+import img4 from '../images/image-4.png';
+
+const ImgCard = () => {
+    const card = [
+        {image:img1,name:"Sam Reaves",info:"I use Udemy for learning specific technologies but chose Builder Book after being impressed with their website being the actual project you build. The up to date, step by step instructions made creating the app a breeze."},
+        {image:img2,name:"Salie Lim",info:"Builder Book is by far the best book I have found on the MERN stack. It also covers testing. Very comprehensive book with explanations so that you actually understand what the code does."},
+        {image:img3,name:"Earl Lee",info:"As an engineer turned product manager that's a few years removed from writing code, Builder Book has been a quick and convenient way to brush up on full-stack web development and learn React for a side project."},
+        {image:img4,name:"The authors are particularly receptive to open-source issues and pull-requests; this made my experience very worthwhile. Strongly recommend to those going from junior to middleweight JavaScript developers"},
+
+    ]
+}
